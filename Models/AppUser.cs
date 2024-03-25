19f56e42302dfbe3eb710533831992cff61d631d
jgrissom <jgrissom91@gmail.com>
2024-03-24 19:18:37 -0500

    [1.12] Disable nullables

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -9,9 +9,9 @@ public class AppUser : IdentityUser
 public class CreateUser
 {
   [Required]
-  public string? Name { get; set; }
+  public string Name { get; set; }
   [Required]
-  public string? Email { get; set; }
+  public string Email { get; set; }
   [Required]
-  public string? Password { get; set; }
+  public string Password { get; set; }
 }
