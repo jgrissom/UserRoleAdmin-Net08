cce8743503f9ae8109f457ad3f2c91c98ba4d1bc
jgrissom <jgrissom91@gmail.com>
2024-03-24 19:11:26 -0500

    [1.10] HTTP Post requests for Home/Create

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,4 +9,30 @@ public class HomeController(UserManager<AppUser> usrMgr) : Controller
 
   public ViewResult Index() => View(userManager.Users);
   public ViewResult Create() => View(new CreateUser());
+  [HttpPost]
+  public async Task<IActionResult> Create(CreateUser model)
+  {
+    if (ModelState.IsValid)
+    {
+      AppUser user = new AppUser
+      {
+        UserName = model.Name,
+        Email = model.Email
+      };
+      IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        foreach (IdentityError error in result.Errors)
+        {
+          ModelState.AddModelError("", error.Description);
+        }
+      }
+    }
+    return View(model);
+  }
 }
